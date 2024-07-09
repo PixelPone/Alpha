8ceb899c73b1072761b0b8de58f948db7faebbb9
GeoDash897 <mattjroman03@gmail.com>
2024-07-09 09:07:04 -0400

    Update structure of game items to differentiate instances of same type
    While ScriptableObjects are good for data containers, it creates the
    issue of multiple items sharing the same data. For most
    variables, this is not a problem (as that is the role of a ScriptableObject),
    but it makes it harder to differentiate multiple items of the same type.
    
    In order to account for that that, an item's data is split between
    BaseItemStats and ItemStats in a similar manner to how ActorStats are structure,
    with ItemStats creating a unique GUID for each item. By using this GUID, it allows
    for a way to differentiate separate instances of items, even if they
    are of the same type.

--- Assets/Scripts/Items/ItemStats.cs
+++ Assets/Scripts/Items/ItemStats.cs
@@ -1,20 +1,16 @@
+﻿using System;
 using UnityEngine;
-using static Constants;
 
-[CreateAssetMenu(fileName = "Dummy_Item", menuName = "Alpha/Item")]
-public class ItemStats : ScriptableObject
+public class ItemStats : MonoBehaviour
 {
-    [field: SerializeField]
-    public string ItemName { get; private set; } = "Dummy Item";
-    [field: SerializeField, TextArea(minLines: 5, maxLines: 5)]
 
-    public string SpecialNotes { get; private set; } = "Test Description!";
     [field: SerializeField]
-    public Item_Rarity ItemRarity { get; private set; }
-    [field: SerializeField]
-    public int Weight { get; private set; }
-    [field: SerializeField]
-    public int Value { get; private set; }
-    [field: SerializeField]
-    public bool IsStackable { get; private set; }
+    public BaseItemStats BaseItemInfo {  get; private set; }
+
+    public string UniqueId { get; private set; }
+
+    private void Awake()
+    {
+        UniqueId = Guid.NewGuid().ToString();
+    }
 }
